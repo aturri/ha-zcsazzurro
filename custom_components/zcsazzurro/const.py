2bb62e5871772e79a8211bbcf7c0c1ed384d1db1
Andrea Turri <andrea.turri92@gmail.com>
2023-04-11 22:21:14 +0200

    bump to 0.0.3

--- custom_components/zcsazzurro/const.py
+++ custom_components/zcsazzurro/const.py
@@ -1,7 +1,7 @@
 """Constants for the ZCS Azzurro integration."""
 
 DOMAIN = "zcsazzurro"
-VERSION = "0.0.2"
+VERSION = "0.0.3"
 API_READ_TIMEOUT = 30
 API_POLL_INTERVAL = 300
 MANUFACTURER = "ZCS Azzurro"
