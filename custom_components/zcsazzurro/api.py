64390d0296a3643314867cc91a4c9f6bfcf093be
Andrea Turri <andrea.turri92@gmail.com>
2023-10-15 10:05:32 +0000

    Improve logging to provide clear explaination of errors obtained from remote endpoint

--- custom_components/zcsazzurro/api.py
+++ custom_components/zcsazzurro/api.py
@@ -2,6 +2,8 @@
 import json
 import logging
 
+import httpx
+
 from homeassistant.components.rest.const import DEFAULT_SSL_CIPHER_LIST
 from homeassistant.components.rest.data import RestData
 from homeassistant.core import HomeAssistant
@@ -9,6 +11,8 @@ from homeassistant.core import HomeAssistant
 from .const import API_READ_TIMEOUT
 
 ZCS_ENDPOINT = "https://third.zcsazzurroportal.com:19003"
+ZCS_502_ERROR = "502 Proxy Error"
+ZCS_503_ERROR = "503 Service Unavailable"
 
 _LOGGER = logging.getLogger(__name__)
 
@@ -45,17 +49,24 @@ class ZCSPortal:
 
         api_result = await self._fetch_data(payload)
         thing_result = {}
+        use_cached_result = False
 
-        if api_result is not None:
+        if api_result[1] is not None:
             try:
-                thing_result = api_result["realtimeData"]["params"]["value"][0][
+                thing_result = api_result[1]["realtimeData"]["params"]["value"][0][
                     self._thing_key
                 ]
             except KeyError:
-                _LOGGER.warning("Unable to read result from ZCS Azzurro portal")
+                _LOGGER.warning(
+                    "No data in response from ZCS Azzurro portal for %s",
+                    self._thing_key,
+                )
+        elif api_result[0] in range(500, 600):
+            use_cached_result = True
 
         result = {}
         result[self._thing_key] = thing_result
+        result["_use_cached_result"] = use_cached_result
         return result
 
     async def _fetch_data(self, payload: dict, timeout: int = API_READ_TIMEOUT):
@@ -83,15 +94,37 @@ class ZCSPortal:
         )
         await rest.async_update()
 
+        if rest.data is None and isinstance(
+            rest.last_exception, httpx.TimeoutException
+        ):
+            _LOGGER.warning(
+                "Timeout fetching data from ZCS Azzurro portal at %s", ZCS_ENDPOINT
+            )
+            return (0, None)
+
         if rest.data is None:
-            _LOGGER.warning("Unable to fetch data from ZCS Azzurro portal")
-            return None
+            message = (
+                "unknown reason"
+                if rest.last_exception is None
+                else str(rest.last_exception)
+            )
+            _LOGGER.error(
+                "Error fetching data from ZCS Azzurro portal, reason is: %s", message
+            )
+            return (400, None)
+
+        if ZCS_502_ERROR in rest.data:
+            _LOGGER.warning("ZCS Azzurro portal is unavailable: %s", ZCS_502_ERROR)
+            return (502, None)
+
+        if ZCS_503_ERROR in rest.data:
+            _LOGGER.warning("ZCS Azzurro portal is unavailable: %s", ZCS_503_ERROR)
+            return (503, None)
 
         try:
-            return json.loads(rest.data)
+            return (200, json.loads(rest.data))
         except json.decoder.JSONDecodeError:
             _LOGGER.warning(
                 "Unable to parse result from ZCS Azzurro portal: %s", rest.data
             )
-
-        return None
+            return (500, None)
